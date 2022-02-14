3bea429e9c84cfada73785811ac58f0be9485e99
nickballphd <nick.ball.phd@gmail.com>
2022-02-13 21:55:23 -0700

    GAS deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-'AKfycbxKCycVicZgfz9G3_mPUGPUcOF6WLqYZgd2prEfwiIkeBqKRZOV1U2at0O8lH_dRZy5sw'
+'AKfycbzx9aXa5HUyIBiXdA-RnT3rqsADJCYRX3qWI2cwjNztJXdCaNZaR1DEQK4GDkzQs8kVPQ'
 )}
